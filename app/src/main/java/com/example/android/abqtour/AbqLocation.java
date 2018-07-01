c4a9420e7996eddf6ee1763c38511f6f319d5181
Stephen Wood <doowtnehpets@gmail.com>
2018-07-01 15:28:39 -0600

    Added some comments.

--- app/src/main/java/com/example/android/abqtour/AbqLocation.java
+++ app/src/main/java/com/example/android/abqtour/AbqLocation.java
@@ -1,5 +1,13 @@
 package com.example.android.abqtour;
 
+/**
+ * Custom class used to store locations for each view
+ * <p>
+ * image stores the view id of the image
+ * name stores the name of the location
+ * description stores a description of the location
+ * address is used to store the address of the location
+ */
 public class AbqLocation {
 
     private int image;
